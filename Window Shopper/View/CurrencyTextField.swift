87f8ac53ebf541aac19ebd0ab106a38084f8deee
Zan Fras <zan.fraas@gmail.com>
2018-03-17 16:29:43 +0100

    add currency subview

--- Window Shopper/View/CurrencyTextField.swift
+++ Window Shopper/View/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.7978649401)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formater = NumberFormatter()
+        formater.numberStyle = .currency
+        formater.locale = .current
+        currencyLabel.text = formater.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -25,7 +40,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2536922089) // White 20% opacity
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+        clipsToBounds = true
         if let placeholder = placeholder {
             let place = NSAttributedString(string: placeholder, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
